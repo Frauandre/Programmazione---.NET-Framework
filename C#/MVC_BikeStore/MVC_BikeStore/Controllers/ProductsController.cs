deaf13bb5c7c3b71560e5eb05ed0e681151b9178
Frauandre <frauandrea2002@gmail.com>
2023-03-02 10:24:45 +0100

    Exercises C#

---
+++ C#/MVC_BikeStore/MVC_BikeStore/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using MVC_BikeStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_BikeStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private BikeStoresContext db = new BikeStoresContext();
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = db.Products.OrderBy(x => x.Product_Name.ToList());
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult ProductsDetails(int id)
+        {
+            return View(db.Products.Find(id));
+        }
+        public ActionResult ElencoProdottiPerMarca(int id)
+        {
+            var products = db.Products.Where(x => x.Brand_Id == id).OrderBy(x => x.Product_Name).ToList();
+            return View("Index", products);
+        }
+    }
+}
---
+++ C#/MVC_BikeStores/MVC_BikeStores/Controllers/BikesStoresController.cs
@@ -0,0 +1,39 @@
+﻿using MVC_BikeStores.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVC_BikeStores.Controllers
+{
+    public class BikesStoresController : Controller
+    {
+        //Connessione al Database
+        private BikeStoresEntities db = new BikeStoresEntities();
+
+        // GET: BikesStores
+
+        //Creo una view in modo tale da visualizzare l'elenco delle bici
+        public ActionResult Index(string cercaBrand)
+        {
+            var products = db.products.ToList();
+
+            var brands = db.brands.Select(x => x.brand_name == cercaBrand).ToList();
+            ViewBag.cercaBrand = new SelectList(brands);
+            
+            //Cerca per Marca
+            if (!string.IsNullOrEmpty(cercaBrand))
+                products = db.products.Where(x => x.brands.brand_name==cercaBrand.ToLower()).ToList();
+            return View(db.products.ToList());
+        }
+
+        //creo una view per vedere tutte le informazioni dei prodotti
+        public ActionResult Details(int id)
+        {
+            return View(db.products.Find(id));
+        }
+
+    }
+}
